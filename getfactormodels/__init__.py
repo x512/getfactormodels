70e7f190acb21134b9d45a3860f809e7bd2a5d19
x512 <x512@pm.me>
2023-12-20 21:09:38 +1100

    typo

--- getfactormodels/__init__.py
+++ getfactormodels/__init__.py
@@ -20,7 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .__main__ import FactorExtractor, get_factors
 from .models import models  # noqa: F401
