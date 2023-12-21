bb2ec144165d7287bd811e634a1b43259aa82101
x512 <x512@pm.me>
2023-12-22 10:57:52 +1100

    version number (0.0.3)

--- getfactormodels/__init__.py
+++ getfactormodels/__init__.py
@@ -20,7 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .__main__ import FactorExtractor, get_factors
 from .models import models  # noqa: F401
