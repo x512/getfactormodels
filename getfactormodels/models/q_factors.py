8a17928abfbf0b303c9d6f5aff501a30ae771259
x512 <x512@pm.me>
2025-12-11 14:53:41 +1100

    feat:added QFactor w2w freq, chore:Cleaned up QFactor models.

--- getfactormodels/models/q_factors.py
+++ getfactormodels/models/q_factors.py
@@ -20,89 +20,98 @@ import pandas as pd
 from getfactormodels.models.base import FactorModel
 from getfactormodels.utils.utils import _process
 
-
 class QFactors(FactorModel):
-    """
-    Download the q or q5 factor models from global-q.org.
+    """Download q-factor data from global-q.org.
 
-    params:
-        frequency (str): The data frequency ('d', 'w', 'm', 'q', 'y'). Defaults to 'm'.
-        start_date (str, datetime): The start date YYYY-MM-DD
-        end_date (str, datetime): The end date YYYY-MM-DD
-        output_file (str):
-        classic (bool): If True, returns the classic 4-factor model (drops R_EG).
-        cache_ttl (int): Time-to-live for cache in seconds (default 86400s/1 day).
+    Args:
+        frequency (str): The data frequency ('d', 'w', 'm', 'q', 'y', or 'w2w'). Default: 'm'.
+        start_date (str, optional): The start date YYYY-MM-DD.
+        end_date (str, optional): The end date YYYY-MM-DD.
+        output_file (str, optional): Optional file path to save to file. Supports csv, pkl.
+        classic (bool, optional): returns the classic 4-factor q-factor model. Default: False.
+        cache_ttl (int, optional): Cached download time-to-live in seconds (default: 86400).
 
-    Sources:
+    Returns:
+        pd.DataFrame: timeseries of factors
 
+    References:
     - Hou, Kewei, Haitao Mo, Chen Xue, and Lu Zhang, 2021, An augmented q-factor model
-      with expected growth, Review of Finance 25 (1), 1-41. (q5 model)
+    with expected growth, Review of Finance 25 (1), 1-41. (q5 model)
     - Hou, Kewei, Chen Xue, and Lu Zhang, 2015, Digesting anomalies: An investment
-      approach, Review of Financial Studies 28 (3), 650-705. (Classic q-factor model)
+    approach, Review of Financial Studies 28 (3), 650-705. (Classic q-factor model)
 
-    Data Source URL: https://global-q.org/factors.html
+    Data Source: https://global-q.org/factors.html
     """
-    # Note: weekly wednesday-to-wednesday needs to be added TODO:
-
     def __init__(self, frequency: str = 'm', classic=False, **kwargs: Any) -> None:
 
-        if frequency not in ["d", "w", "m", "q", "y"]: # TODO: handle this in base, it's all freqs
-            raise ValueError("Frequency must be 'd', 'w', 'm', 'q' or 'y'")
+        if frequency not in ["d", "w", "w2w", "m", "q", "y"]: # TODO: handle freqs in base model
+            raise ValueError("Frequency must be 'd', 'w', 'm', 'q', 'y',"
+                "or 'w2w' (weekly, Wednesday-to-Wedneday)")
         self.classic = classic 
         super().__init__(frequency=frequency, classic=classic, **kwargs)
 
     def _get_url(self) -> str:
-        file = {'m': "monthly",
+        file = {'m': "monthly", 
                 "d": "daily",
-                "q": "quarterly",
+                "q": "quarterly", 
                 "w": "weekly",
+                "w2w": "weekly_w2w",
                 "y": "annual", }.get(self.frequency)
 
-        url = f'https://global-q.org/uploads/1/2/2/6/122679606/q5_factors_{file}_2024.csv' # TODO: YEAR
+        url = 'https://global-q.org/uploads/1/2/2/6/122679606'
+        url += f'/q5_factors_{file}_2024.csv' # TODO: YEAR
         return url
 
     def download(self) -> pd.DataFrame:
         _data = self._download()
         data = self._read(_data)
-        return data
+        df = self._parse_q_factors(data)
+
+        return df
 
     def _read(self, data) -> pd.DataFrame:
         _file = io.StringIO(data.decode('utf-8'))
-
         index_cols = [0, 1] if self.frequency in ["m", "q"] else [0]
         data = pd.read_csv(_file, parse_dates=False, index_col=index_cols, float_precision="high")
 
-        if self.classic:
-            data = data.drop(columns=["R_EG"])
+        return data
 
-        data = data.rename(columns={"R_F": "RF"})
-        data = data * 0.01
-
-        if self.frequency in ["m", "q"]:
-            # Need to insert "-" (monthly) or "Q" (quarterly) into date str.
+    def _parse_q_factors(self, data) -> pd.DataFrame:
+        if self.frequency in ["m", "q"]: 
             data = data.reset_index()
+
+            # Need to insert "-" or "Q" into date monthly/quarterly str.
             col = "quarter" if self.frequency == "q" else "month"
             char = "q" if self.frequency == "q" else "-"
 
+            # Combines year, period cols into a PeriodIndex to Timestamp
             data["date"] = pd.PeriodIndex(
-                data["year"].astype(str)
-                    + char
-                    + data[col].astype(str), freq=self.frequency.upper()  #fix:FutureWarning 'm' is deprecated, use 'M'
+                data["year"].astype(str) + char + data[col].astype(str),
+                freq=self.frequency.upper()  #fix:FutureWarning 'm' is deprecated, use 'M'
             ).to_timestamp(how="end")
 
             data["date"] = data["date"].dt.normalize()
+
             data = data.drop(["year", col], axis=1).set_index("date")
 
-        if self.frequency == "y":
-            data.index = pd.to_datetime(data.index.astype(str)) \
-                + pd.offsets.YearEnd(0)
-        elif self.frequency not in ["m", "q"]:
+        elif self.frequency == "y":
+            data.index = pd.to_datetime(data.index.astype(str)) + pd.offsets.YearEnd(0)
+
+        else: # daily, weekly
             data.index = pd.to_datetime(data.index.astype(str))
 
         data.columns = data.columns.str.upper()
         data.index.name = "date"
-        data = data.rename(columns={"R_MKT": "Mkt-RF"})  # check: its market, not mkt-rf
 
+        if self.classic:
+            data = data.drop(columns=["R_EG"])
+
+        # TODO: check: its market, not mkt-rf!
+        data = data.rename(columns={"R_MKT": "Mkt-RF", "R_F": "RF"})
+
+        data = data * 0.01
+
+        #TODO: slicing, saving, validating date to base model? util? Use here.
         return _process(data, self.start_date, self.end_date,
                         filepath=self.output_file)
 
