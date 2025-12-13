754fbf19a69ed0db5a89036fb53f671749a2b2d7
x512 <x512@pm.me>
2025-12-13 13:53:54 +1100

    version update:v0.0.8

--- getfactormodels/__init__.py
+++ getfactormodels/__init__.py
@@ -14,10 +14,10 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 import logging
 
-logging.getLogger('getfactormodels').setLevel(logging.DEBUG)
+logging.getLogger('getfactormodels').setLevel(logging.ERROR)
 
 from .main import FactorExtractor, get_factors
 from .models import (BarillasShankenFactors, CarhartFactors, DHSFactors,
