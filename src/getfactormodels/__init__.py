1227d911bc331bad2664d06382810a71b174aa87
x512 <x512@pm.me>
2026-01-22 02:36:32 +1100

    Bump version to 0.0.18

--- src/getfactormodels/__init__.py
+++ src/getfactormodels/__init__.py
@@ -42,7 +42,7 @@ logger.addHandler(logging.NullHandler())
 # see: https://docs.python.org/3/howto/logging-cookbook.html#logging-to-multiple-destinations
 # or use warnings??
 
-__version__ = "0.0.16"
+__version__ = "0.0.18"
 __all__ = [ 
     "get_factors",
     "BABFactors",
